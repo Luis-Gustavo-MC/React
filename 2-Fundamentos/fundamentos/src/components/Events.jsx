801c7d691089facf70770b1bb3c595477382dae6
= <lgmc120@gmail.com>
2026-04-01 13:56:40 -0300

    Fundamentos - end

--- 2-Fundamentos/fundamentos/src/components/Events.jsx
+++ 2-Fundamentos/fundamentos/src/components/Events.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
 const events = () => {
+
+  const handleClick = () => {
+    console.log("Executou!")
+  }
+  const renderSomething = (x)=>{
+      if(x){
+        return <h1>Renderizar isso!</h1>
+      }
+      else{
+        return <h1>Renderizando outras coisas</h1>
+      }
+  }
+  
   return (
     <div>
       <div>
         <button onClick={() => console.log("Hello Word!")}>Clique Aqui!</button>
       </div>
+      <div>
+        <button onClick={handleClick}>Clique Aqui - com função</button>
+      </div>
+      {/* Função render */}
+      {renderSomething(false)}
+      {renderSomething(true)}
     </div>
   )
 }
