49c6d1db3e7872057f980ecab6f2de84d523e25b
= <lgmc120@gmail.com>
2026-04-01 14:58:58 -0300

    Avançando - pt1

---
+++ 3 - Avançando/avançando/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+//  2 - Imagem em assets
+import night from './assets/night.jpg'
+// 3- UseState
+import Data from './components/Data'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Avançando em React</h1>
+        {/* 1 - Imagem em public */}
+        <img src="/street.jpg" alt="Street"/>
+        {/* 2 - Imagem em assets */}
+        <img src={night} alt="Night"/>
+        {/* 3 -useState */}
+        <Data />
+    </>
+  )
+}
+export default App
